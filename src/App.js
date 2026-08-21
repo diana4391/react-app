975dc4bbd418e297010b7a54d7ccd7aa05addbd4
Diana <mm987416013@gmail.com>
2026-08-21 23:18:56 +0800

    Test with absolute path /react-app/test-image.jpg

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <p>This page is deployed with GitHub Pages.</p>
 
       <img
-        src={process.env.PUBLIC_URL + "/test-image.jpg"}
+        src="/react-app/test-image.jpg"
         alt="My work"
         style={{ width: "100%", borderRadius: "12px", marginTop: "100px" }}
       />
